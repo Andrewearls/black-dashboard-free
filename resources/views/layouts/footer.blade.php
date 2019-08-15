81a4750e2b150ca1fb6d858a01f7e7d2f8957123
Andrew Earls <andrewearls@rocketmail.com>
2019-08-15 16:34:59 -0700

    fixing formatting

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -25,7 +25,7 @@
         <div class="copyright">
             &copy; {{ now()->year }} {{ __('made with') }} <i class="tim-icons icon-heart-2"></i> {{ _('by') }}
             <a href="https://creative-tim.com" target="_blank">{{ __('Creative Tim') }}</a> &amp;
-            <a href="https://updivision.com" target="_blank">{{ __('Updivision') }}</a> {{ _('for a better web') }}.
+            <a href="https://updivision.com" target="_blank">{{ __('Updivision') }}</a> {{ __('for a better web') }}.
         </div>
     </div>
 </footer>
